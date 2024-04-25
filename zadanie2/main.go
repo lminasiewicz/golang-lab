15e40d6516a21d329715f0551281b700fe461335
Łukasz Minasiewicz <l.minasiewicz.027@studms.ug.edu.pl>
2024-04-25 15:05:42 +0200

    finished basic simulation, TODO: fix edge case with lightning missing despite hitting a tree, implement multiple simulations and gather stats

--- zadanie2/main.go
+++ zadanie2/main.go
@@ -1,13 +1,26 @@
 package main
 
-import ("fmt";
-		"math/rand/v2")
+import (
+	"fmt";
+	"math/rand/v2"
+	"math"
+)
 
 type Field struct {
 	tree bool
 	age int
 	burned bool
 }
+
+// pseudo-enum na kierunki wiatru
+type Direction int
+const (
+	None Direction = iota
+	North 
+	East
+	South
+	West
+)
 
 const MAX_AGE = 600
 const BURN_RESISTANCE_THRESHOLD = 400
@@ -50,10 +63,27 @@ func initialize_forest(width int, length int, rate float32) [][]Field {
 	return forest
 }
 
-func get_coords_around(forest [][]Field, coords [2]int) [][]int {
-	result := make([][]int, 0, 8)
-	for i := -1; i < 2; i++ {
-		for j := -1; j < 2; j++ {
+func get_coords_around(forest [][]Field, coords [2]int, wind Direction) [][]int {
+	result := make([][]int, 0, 11)
+	i_start, j_start := -1, -1
+	i_end, j_end := 2, 2
+
+	switch wind {
+	case North:
+		i_start -= 1
+		break
+	case East:
+		j_end += 1
+		break
+	case South:
+		i_end += 1
+		break
+	case West:
+		j_start -= 1
+		break
+	}
+	for i := i_start; i < i_end; i++ {
+		for j := j_start; j < j_end; j++ {
 			if coords[0] + i >= 0 && coords[0] + i < len(forest) && coords[1] + j >= 0 && coords[1] + j < len(forest[0]) {
 				if !(i == 0 && j == 0) {
 					result = append(result, []int{coords[0]+i, coords[1]+j})
@@ -64,41 +94,113 @@ func get_coords_around(forest [][]Field, coords [2]int) [][]int {
 	return result
 }
 
-func burn(forest [][]Field, current [2]int) {
-	available_spaces := get_coords_around(forest, current)
+func burn(forest [][]Field, current [2]int, wind Direction) {
+	available_spaces := get_coords_around(forest, current, wind)
+	next_burners := make([][2]int, 0, 11)
 	for _, space := range available_spaces {
 		fld := forest[space[0]][space[1]]
 		if fld.tree && !fld.burned {
 			if attempt_burn(fld.age) {
-				fld.burned = true
+				forest[space[0]][space[1]].burned = true
 				space_arr := [2]int{space[0], space[1]}
-				burn(forest, space_arr)
+				next_burners = append(next_burners, space_arr)
 			}
 		}
 	}
+	for _, burner := range next_burners {
+		burn(forest, burner, wind)
+	}
 }
 
-func print_forest(forest [][]Field) {
+func lightning_strike(forest [][]Field, lightning [2]int, wind Direction) {
+	fld := forest[lightning[0]][lightning[1]]
+	if fld.tree && !fld.burned {
+		fld.burned = true
+		burn(forest, lightning, wind)
+	}
+}
+
+func lightning_strike_until_successful(forest [][]Field, wind Direction) [2]int {
+	lightning := [2]int{rand.IntN(len(forest)), rand.IntN(len(forest[0]))}
+	for {
+		lightning = [2]int{rand.IntN(len(forest)), rand.IntN(len(forest[0]))}
+		fld := forest[lightning[0]][lightning[1]]
+		if fld.tree && !fld.burned {
+			lightning_strike(forest, lightning, wind)
+			break
+		}
+	}
+	return lightning
+}
+
+func print_forest(forest [][]Field, lightning [2]int) {
 	for row := 0; row < len(forest); row++ {
-		for _, fld := range forest[row] {
+		for elem := 0; elem < len(forest[0]); elem++ {
+			fld := forest[row][elem]
+			
 			if !fld.tree {
-			fmt.Print(" ")
+				if row == lightning[0] && elem == lightning[1] {
+					fmt.Print("★")
+				} else {
+					fmt.Print(" ")
+				}
 			} else if fld.tree && !fld.burned {
-				fmt.Print("◯")
+				fmt.Print("○︎")
 			} else if fld.tree && fld.burned {
-				fmt.Print("⬤")
+				if row == lightning[0] && elem == lightning[1] {
+					fmt.Print("✪")
+				} else {
+					fmt.Print("●︎")
+				}
 			}
 		}
 		fmt.Println()
 	}
+	fmt.Println()
+}
+
+func print_forest_stats(forest [][]Field) {
+	// Funkcja do wypisywania statystyk dot. stanu lasu po trafieniu piorunem
+	trees := 0
+	burned := 0
+	for row := 0; row < len(forest); row++ {
+		for _, fld := range forest[row] {			
+			if fld.tree {
+				trees += 1
+				if fld.burned {
+					burned += 1
+				}
+			}
+		}
+	}
+	length := len(forest[0])
+	width := len(forest)
+	forestation := math.Round(10000 * (float64(trees) / float64(length * width))) / 100
+	burned_rate := math.Round(10000 * (float64(burned) / float64(trees))) / 100
+	fmt.Print("Forest ", length, "x", width, " (", length*width, " spaces)\n")
+	fmt.Print("Forestation: ", forestation, "%\n")
+	fmt.Print("Trees: ", trees, "  |  Burned: ", burned, "\n")
+	fmt.Print(burned_rate, "% of the forest has been burned.\n")
+	if burned == 0 {
+		fmt.Println("Lightning Strike missed.")
+	}
+}
+
+func simulate_once(length int, width int, forestation_rate float32, wind Direction, lightning_is_accurate bool) {
+	forest := initialize_forest(width, length, forestation_rate)
+
+	var lightning = [2]int{}
+	if lightning_is_accurate {
+		lightning = lightning_strike_until_successful(forest, wind)
+	} else {
+		lightning = [2]int{rand.IntN(width), rand.IntN(length)}
+		lightning_strike(forest, lightning, wind)
+	}
+	print_forest(forest, lightning)
+	print_forest_stats(forest)
+	fmt.Println(lightning)
 }
 
 func main() {
-	length := 20
-	width := 10
-	forest := initialize_forest(width, length, 0.3)
-	lightning := [2]int{rand.IntN(width), rand.IntN(length)}
-	fmt.Println(lightning)
-	burn(forest, lightning)
-	print_forest(forest)
+	simulate_once(40, 20, 0.4, None, false)
 }
