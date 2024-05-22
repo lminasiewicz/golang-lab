36b2ebf58eca1b97c3cef9c874e7333f1c6fcdbd
Łukasz Minasiewicz <l.minasiewicz.027@studms.ug.edu.pl>
2024-05-22 15:45:11 +0200

    reworking the first endpoing (/entries)

--- zadanie3/server.go
+++ zadanie3/server.go
@@ -5,13 +5,13 @@ import (
 	"net/http";
 	"os";
 	"io";
-	"encoding/json",
-	"sync",
+	"encoding/json";
+	"sync";
 	"math/rand"
 )
 
 
-var mutex := sync.Mutex
+var mutex sync.Mutex
 
 
 type Entry struct {
@@ -24,8 +24,8 @@ type Data struct {
     Type     string `json:"Type"`
     Country  string `json:"Country"`
     Activity string `json:"Activity"`
-	Sex      string `json:Sex`
-	Fatal    string `json:Fatal_y_n`
+	Sex      string `json:"Sex"`
+	Fatal    string `json:"Fatal_y_n"`
 }
 
 
@@ -34,6 +34,9 @@ func get_db() []Entry {
     if err != nil {
         fmt.Println(err)
     }
+	defer jsonFile.Close()
+
+	byteValue, _ := io.ReadAll(jsonFile)
 
     var jsonList []Data
     json.Unmarshal(byteValue, &jsonList)
@@ -48,50 +51,52 @@ func get_db() []Entry {
 	return db
 }
 
+func choose_random_ten(db []Entry) []Entry {
+	len_db := len(db)
+	random_post_ids := [10]int{}
+	for i := 0; i < len(random_post_ids); i++ {
+		for {
+			unique := true
+			index := rand.Intn(len_db-1)
+			for _, elem := range random_post_ids {
+				if index == elem {
+					unique = false
+					break
+				}
+			}
+			if unique == true {
+				random_post_ids[i] = index
+				break
+			}
+		}
+	}
+
+	random_posts := make([]Entry, 10)
+	for i := 0; i < len(random_posts); i++ {
+		random_posts[i] = db[random_post_ids[i]]
+	}
+	return random_posts
+}
+
 
 func entriesHandler (w http.ResponseWriter, r *http.Request) {
 	if r.Method == "GET" {
 		mutex.Lock()
 		defer mutex.Unlock()
 
-		db := get_db()
-		len_db := len(db)
-		random_post_ids := [10]int{}
-		for i := 0; i < len(random_post_ids), i++ {
-			for {
-				unique := true
-				index := rand.Intn(len_db-1)
-				for _, elem := range random_post_ids {
-					if index == elem {
-						unique = false
-						break
-					}
-				}
-				if unique == true {
-					random_post_ids[i] = index
-					break
-				}
-			}
-		}
-
-		random_posts := make([]Entry, 10)
-		for i := 0; i < len(random_posts); i++ {
-			random_posts[i] = db[random_post_ids[i]]
-		}
 		w.Header().Set("Content-type", "application/json")
 		json.NewEncoder(w).Encode(random_posts)
-	}
-	else {
+	} else {
 		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
 	}
 }
 
 
 func main() {
-	// Get database from file
-	db := get_db()
+	db := choose_random_ten(get_db())
 
 	http.HandleFunc("/entries", entriesHandler)
-	http.ListenAndserve(":8080", nil)
+
 	fmt.Println("Server is running at http://localhost:8080")
+	http.ListenAndServe(":8080", nil)
 }
